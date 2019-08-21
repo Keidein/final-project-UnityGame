2fe44a52b79e702d91ed64fad642918d72002d66
rethavanderwalt <42917205+rethavanderwalt@users.noreply.github.com>
2019-08-22 08:33:43 +1200

    file changes

---
+++ Under Pressure/Assets/Scripts/LevelTimer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelTimer : MonoBehaviour {
+
+	public float levelStartTime;
+
+	private Text theText;
+
+	//private PauseMenu thePauseMenu;
+
+	// Use this for initialization
+	void Start () {
+		theText = GetComponent<Text>();
+
+		//thePauseMenu = FindObjectOfType<PauseMenu>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/* if (thePauseMenu.isPaused)
+			return; */
+
+		levelStartTime -= Time.deltaTime;
+		theText.text = "" + Mathf.Round (levelStartTime);
+	}
+}
