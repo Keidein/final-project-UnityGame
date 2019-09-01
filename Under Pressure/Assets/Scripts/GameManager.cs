051432c4c2bbfef4303c4688d908960bfc1f1717
Ethan-Meleah <40743637+Ethan-Meleah@users.noreply.github.com>
2019-09-02 10:09:01 +1200

    added-mechanic
    mechanics and test rooms added to the project. Built on Retha UI

---
+++ Under Pressure/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject gameOverMenuUI;
+	bool gameHasEnded = false;
+	public float restartDelay = 5f;
+
+	public void EndGame ()
+	{
+		if (gameHasEnded == false)
+		{
+			gameHasEnded = true;
+			Debug.Log("GAME OVER");
+			GameOver();
+			//Invoke("Restart", restartDelay);
+		}	
+
+
+	}
+
+
+	public void GameOver ()
+	{
+		gameOverMenuUI.SetActive(true);
+		Time.timeScale = 0f;
+	}
+
+	public void Restart ()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		Time.timeScale = 1f;
+	}
+
+	public void LoadMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+		Time.timeScale = 1f;
+	}
+}
